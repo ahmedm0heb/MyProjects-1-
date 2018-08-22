5669854e20d2f033a2d156b5648882ac1f542859
ahmedm0heb <ahmed_mohab10@yahoo.com>
2018-08-22 16:05:48 +0300

    initial commit

---
+++ js/plugins.js
@@ -0,0 +1,10 @@
+"use strict";
+function onLoading(){
+var par1 = document.getElementById('pContent');
+par1.classList.remove('hidden');
+}
+function Contact()
+{
+	var cont = document.getElementById('containerForm');
+	cont.classList.remove('hidden');
+}
